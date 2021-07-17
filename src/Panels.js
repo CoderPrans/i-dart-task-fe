b333c724ad67df400ada6ab910bd77f81dbbcc75
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2021-07-17 13:12:39 +0530

    design updates

--- src/Panels.js
+++ src/Panels.js
@@ -15,29 +15,71 @@ export default function Panels({ d }) {
   return (
     <div className="list">
       <div className="card">
-        {d.Script}
-        <br />
-        {d.Price}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            {d.Script}
+            <br />
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "dodgerblue",
+                fontSize: "18px",
+              }}
+            >
+              {d.Price}
+            </span>
+          </div>
+          <div style={{ textAlign: "left" }}>
+            <span style={{ color: "#FBB36B", fontWeight: "bold" }}>
+              iShares
+            </span>
+            <br />
+            <span style={{ fontSize: "9px" }}>by BlackDox</span> <br />
+            <span style={{ fontWeight: "bold" }}>S&P 500 Index</span>
+            <br /> <span style={{ fontSize: "12px" }}>US Equity</span>
+          </div>
+        </div>
       </div>
       <div className="card">
         <li>
-          <div>Quantity</div> <div>{quantity}</div>
+          <div>
+            <i class="fa fa-hashtag"></i> &nbsp; Quantity
+          </div>{" "}
+          <b>{quantity}</b>
         </li>
         <li>
-          <div>Avg. Cost</div> <div>{d["Avg. Cost"]}</div>
+          <div>
+            <i style={{ fontWeight: "bold" }} class="fa fa-at"></i> &nbsp; Avg.
+            Cost
+          </div>{" "}
+          <b>{d["Avg. Cost"]}</b>
         </li>
         <li>
-          <div>Invested Amt</div> <div>{d["Invested Amount"]}</div>
+          <div>
+            <i class="fa fa-money"></i> &nbsp; Invested Amt
+          </div>{" "}
+          <b>{d["Invested Amount"]}</b>
         </li>
       </div>
       <div className="card">
-        <li>
-          <span>Market Value:</span> <span>{market_value}</span>
-        </li>
-        <li>
-          <span>% of Portfolio Value:</span>
-          <span>{d["% of Portfolio Value"]}</span>
-        </li>
+        <div>
+          <li>
+            <span style={{ fontWeight: "bold" }}>Market Value:</span>
+            <span style={{ fontWeight: "bold" }}>${market_value}</span>
+          </li>
+          <li>
+            <span>% of Portfolio Value:</span>
+            <span style={{ fontWeight: "bold" }}>
+              {d["% of Portfolio Value"]}
+            </span>
+          </li>
+        </div>
         <br />
 
         <p className="groove">
@@ -48,32 +90,64 @@ export default function Panels({ d }) {
         </p>
       </div>
       <div className="card">
-        <li>
-          <span>Unrealized P&L:</span>{" "}
-          <span>
-            {d["Unrealized P&L"][1] === "-" ? "0" : d["Unrealized P&L"]}
-          </span>
-        </li>
-        <li>
-          <span>% Retun:</span> <span>{d["% Return"]}</span>
-        </li>
-
-        <p className="groove return" style={{ position: "absolute" }}>
-          <p
-            className="line"
-            style={{
-              position: "relative",
-              top: "50%",
-              left: "50%",
-              transform:
-                d["% Return"][0] === "-"
-                  ? "translate(-100%, -50%)"
-                  : "translate(0%, -50%)",
-              background: d["% Return"][0] === "-" ? "orangered" : "limegreen",
-              width: (percent_return * 200) / 200,
-            }}
-          ></p>
-        </p>
+        <div>
+          <li>
+            <span style={{ fontWeight: "bold" }}>Unrealized P&L:</span>{" "}
+            <span style={{ fontWeight: "bold" }}>
+              {d["Unrealized P&L"][1] === "-" ? "0" : d["Unrealized P&L"]}
+            </span>
+          </li>
+          <li>
+            <span>% Retun:</span>{" "}
+            <span>
+              {" "}
+              {d["% Return"][0] !== "-" ? (
+                <>
+                  <i class="fa fa-caret-up" style={{ color: "limegreen" }}></i>{" "}
+                  <b>{d["% Return"]}</b>
+                </>
+              ) : (
+                <>
+                  <i
+                    class="fa fa-caret-down"
+                    style={{ color: "orangered" }}
+                  ></i>{" "}
+                  <b>{d["% Return"].slice(1)}</b>
+                </>
+              )}
+            </span>
+          </li>
+        </div>
+        <div
+          style={{
+            height: "40px",
+            position: "relative",
+            left: "10px",
+            bottom: "-10px",
+          }}
+        >
+          <p className="groove return" style={{ position: "absolute" }}>
+            <p
+              className="line"
+              style={{
+                position: "relative",
+                top: "50%",
+                left: "50%",
+                transform:
+                  d["% Return"][0] === "-"
+                    ? "translate(-100%, -50%)"
+                    : "translate(0%, -50%)",
+                background:
+                  d["% Return"][0] === "-" ? "orangered" : "limegreen",
+                borderRadius:
+                  d["% Return"][0] === "-"
+                    ? "8px 0px 0px 8px"
+                    : "0px 8px 8px 0px",
+                width: (percent_return * 200) / 200,
+              }}
+            ></p>
+          </p>
+        </div>
       </div>
     </div>
   );
